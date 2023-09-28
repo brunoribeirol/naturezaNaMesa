d44b264ae3f3e6364fecfc8beb2336cdc561d3ce
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-28 20:15:21 -0300

    .

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/brunoribeiro/Desktop/naturezaNaMesa/venv"
+set -gx VIRTUAL_ENV "/Users/brunoribeiro/Documents/GitHub/naturezaNaMesa/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
