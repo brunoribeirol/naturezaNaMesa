e8b431bb059013c79b0751db7c2c46f2a6057c70
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-28 22:41:06 -0300

    Uptaded the Urls and generated the templates

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'app/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
