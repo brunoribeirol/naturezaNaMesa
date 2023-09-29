05365a8da55961979782dd0aaf8eb5e65a8b43b5
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-09-29 18:10:48 -0300

    Index pages

--- naturezaNaMesa/naturezaApp/views.py
+++ naturezaNaMesa/naturezaApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def naturezaApp(request):
+    context = {}
+    return render(request, 'naturezaApp/naturezaApp.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'naturezaApp/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'naturezaApp/checkout.html', context)
+
+
+
