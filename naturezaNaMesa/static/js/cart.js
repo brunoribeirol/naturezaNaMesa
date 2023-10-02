2ebf3d109dc51f6ff699a3f201f92e536a34c549
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-10-01 22:38:26 -0300

    Added the functionality to add and remove to the cart

--- naturezaNaMesa/static/js/cart.js
+++ naturezaNaMesa/static/js/cart.js
@@ -21,20 +21,23 @@ function updateUserOrder(productId, action) {
   var url = "/update_item/";
 
   fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
+      'X-CSRFToken': csrftoken,
     },
     body: JSON.stringify({
-      productId: productId,
-      action: action,
+      'productId': productId,
+      'action': action,
     }),
   })
+
     .then((response) => {
       return response.json();
     })
 
     .then((data) => {
-      console.log("data:", data);
+      console.log('data:', data);
+      location.reload();
     });
 }
