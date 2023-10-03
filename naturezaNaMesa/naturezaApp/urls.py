09c43cc6b668f45bef1adb160ab6da97c8125381
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-10-02 23:11:23 -0300

    Version 1.0 2/10/23

--- naturezaNaMesa/naturezaApp/urls.py
+++ naturezaNaMesa/naturezaApp/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
 
+    path('entrada/', views.entrada, name='entrada'),
+    path('produto/<int:product_id>/', views.detalhes_produto, name='detalhes_produto'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login, name='login'),
+
     path('update_item/', views.updateItem, name='update_item')
 ]
