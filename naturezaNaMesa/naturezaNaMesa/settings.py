2a71895fd10ab9c0ab589b8cdaaf6bb3d03a7ad1
Bruno Ribeiro <89156916+brunoribeirol@users.noreply.github.com>
2023-10-04 20:37:38 -0300

    Update settings.py

--- naturezaNaMesa/naturezaNaMesa/settings.py
+++ naturezaNaMesa/naturezaNaMesa/settings.py
@@ -98,13 +98,6 @@ WSGI_APPLICATION = 'naturezaNaMesa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
