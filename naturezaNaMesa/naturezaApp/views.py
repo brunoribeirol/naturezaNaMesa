d6aa79ea39dc44fbc3de3ad505de7a510709d405
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2023-10-13 18:26:27 -0300

    .

--- naturezaNaMesa/naturezaApp/views.py
+++ naturezaNaMesa/naturezaApp/views.py
@@ -1,9 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
-from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 import json
 
@@ -11,7 +6,10 @@ from .models import *
 
 # Create your views here.
 
+<<<<<<< HEAD
 #@login_required(login_url='login/')   
+=======
+>>>>>>> parent of 09c43cc (Version 1.0 2/10/23)
 def naturezaApp(request):
 
     if request.user.is_authenticated:
@@ -83,53 +81,3 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse("O item foi adicionado", safe=False)
-
-def entrada(request):
-    context = {}
-    return render(request, 'naturezaApp/entrada.html', context)
-
-def cadastro(request):
-    if request.method=="GET":
-        return render(request, 'naturezaApp/cadastro.html')
-    else: 
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        senha=request.POST.get('senha')
-
-        user = User.objects.filter(username=username).first()
-
-        if user:
-            return HttpResponse("jÃ¡ existe um usuario com esse username")
-        
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        user.save()
-        
-        return HttpResponse("usuario cadastrado com sucesso")
-
-def login(request):
-    if request.method == "GET":
-        return render(request, 'naturezaApp/login.html')
-    else: 
-        username = request.POST.get('username')
-        senha= request.POST.get('senha')
-
-        user = authenticate(username=username, password=senha)
-
-        if user:
-            login_django(request, user)
-            return HttpResponse("autenticado")
-        else:
-            return HttpResponse("usuario ou senha invalidos")
-
-
-def plataforma(request):
-    if request.user.is_authenticated: 
-        return HttpResponse('plataforma')
-    return HttpResponse('vc precisa logar')
-
-from django.shortcuts import render, get_object_or_404
-from .models import Product
-
-def detalhes_produto(request, product_id):
-    produto = get_object_or_404(Product, id=product_id)
-    return render(request, 'naturezaApp/detalhes_produto.html', {'produto': produto})
